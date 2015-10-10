babc7844b45a6fcff842c4dc1367ce014b17be72
Holmes <holmes_holmes@live.com>
2015-10-10 14:06:13 -0300

    changes packages-jwm multimedia.sh

--- jwm-overlay/etc/skel/.scripts/multimedia.sh
+++ jwm-overlay/etc/skel/.scripts/multimedia.sh
@@ -13,6 +13,6 @@ Press ${grnb}<enter>${nrml} to proceed. You may still cancel the process when pr
 
 read
 pacman -S gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer0.10-bad-plugins gstreamer0.10-base-plugins gstreamer0.10-good-plugins
-gstreamer0.10-ugly-plugins flashplugin libdvdcss
+gstreamer0.10-ugly-plugins flashplugin libdvdcss flac
 read -p $'\n'"Process Complete. Press ${grnb}<enter>${nrml} to continue."$'\n'
 exit 0
