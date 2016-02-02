e940006b688e0a2c42ff595e87f7536d6c1dc8c7
Holmes <holmes_holmes@live.com>
2016-02-02 00:06:42 -0200

    changes various

--- jwm-overlay/etc/skel/.battery/battery.sh
+++ jwm-overlay/etc/skel/.battery/battery.sh
@@ -2,13 +2,13 @@ a=`ibam --percentbattery | head --lines 1`
 set $a
 flag=0
 
-if [ $3 -eq 80 -a $flag -eq 0 ]
+if [ $3 -eq 90 -a $flag -eq 0 ]
 then
 notify-send "Battery is full" " Remove the charger and save electricity!"
 flag=1
 #aplay -q full.wav 
 
-elif [ $3 -le 40 -a $flag -eq 0 ]
+elif [ $3 -le 20 -a $flag -eq 0 ]
 then
 notify-send "Battery low" "Please charge!"
 flag=1
